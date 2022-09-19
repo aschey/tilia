f590792497d5da28ff17cad6a49121d889dc6904
Austin Schey <aschey13@gmail.com>
2022-09-19 09:20:36 -0500

    lazy load

--- src/state.rs
+++ src/state.rs
@@ -1,10 +1,16 @@
-use std::sync::atomic::AtomicBool;
-
-use once_cell::sync::OnceCell;
-
 use crate::command::Command;
+use once_cell::sync::OnceCell;
+use std::sync::{atomic::AtomicU16, RwLock};
 
-pub(crate) static IS_INITIALIZED: AtomicBool = AtomicBool::new(false);
-pub(crate) static IS_CONNECTED: AtomicBool = AtomicBool::new(false);
+pub(crate) struct ConnectState;
+
+impl ConnectState {
+    pub(crate) const NOT_CONNECTED: u16 = 1;
+    pub(crate) const CONNECTED: u16 = 2;
+    pub(crate) const DISABLED: u16 = 3;
+}
+
+pub(crate) static IS_INITIALIZED: RwLock<bool> = RwLock::new(false);
+pub(crate) static CONNECT_STATE: AtomicU16 = AtomicU16::new(ConnectState::DISABLED);
 pub(crate) static SENDER: OnceCell<tokio::sync::mpsc::Sender<Command>> = OnceCell::new();
 pub(crate) static HANDLE: OnceCell<tokio::task::JoinHandle<()>> = OnceCell::new();
