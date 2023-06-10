05a70168f47fe752dc465674bd314bbea136e1b4
Austin Schey <aschey13@gmail.com>
2023-06-10 15:48:16 -0500

    switch to vecdeque

--- crates/tilia/src/history.rs
+++ crates/tilia/src/history.rs
@@ -1,43 +1,57 @@
-use crossbeam::queue::ArrayQueue;
-use std::sync::Arc;
+use std::{
+    collections::VecDeque,
+    sync::{Arc, RwLock},
+};
 use tokio::sync::broadcast;
 
 pub fn channel(capacity: usize) -> Sender {
     let (tx, _) = broadcast::channel(capacity);
     Sender {
-        buf: Arc::new(ArrayQueue::new(capacity)),
+        size: 0,
+        capacity,
+        buf: Arc::new(RwLock::new(VecDeque::with_capacity(capacity))),
         tx,
     }
 }
 
 #[derive(Clone)]
 pub struct Sender {
-    buf: Arc<ArrayQueue<Vec<u8>>>,
+    size: usize,
+    capacity: usize,
+    buf: Arc<RwLock<VecDeque<Vec<u8>>>>,
     tx: broadcast::Sender<Vec<u8>>,
 }
 
 impl Sender {
     pub fn send(&mut self, value: Vec<u8>) -> Result<usize, broadcast::error::SendError<Vec<u8>>> {
-        self.buf.force_push(value.clone());
-        self.tx.send(value)
+        let mut buf = self.buf.write().expect("Lock poisoned");
+        if self.size >= self.capacity {
+            buf.pop_front();
+        }
+        buf.push_back(value.clone());
+        let res = self.tx.send(value);
+        if self.size < self.capacity {
+            self.size += 1;
+        }
+        res
     }
 
     pub fn subscribe(&self) -> Receiver {
         Receiver {
-            buf: self.buf.clone(),
+            buf: self.buf.read().expect("Lock poisoned").clone(),
             rx: self.tx.subscribe(),
         }
     }
 }
 
 pub struct Receiver {
-    buf: Arc<ArrayQueue<Vec<u8>>>,
+    buf: VecDeque<Vec<u8>>,
     rx: broadcast::Receiver<Vec<u8>>,
 }
 
 impl Receiver {
     pub async fn recv(&mut self) -> Result<Vec<u8>, broadcast::error::RecvError> {
-        if let Some(val) = self.buf.pop() {
+        if let Some(val) = self.buf.pop_front() {
             Ok(val)
         } else {
             self.rx.recv().await
