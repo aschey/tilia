12151393570c2633162179f87b4096ac54b55d1e
Austin Schey <aschey13@gmail.com>
2024-07-26 17:35:54 -0700

    remove once_cell

--- crates/tilia/src/state.rs
+++ crates/tilia/src/state.rs
@@ -1,10 +1,9 @@
 use std::sync::atomic::AtomicBool;
-use std::sync::RwLock;
+use std::sync::{OnceLock, RwLock};
 
 use background_service::BackgroundServiceManager;
-use once_cell::sync::OnceCell;
 use tokio::sync::Mutex;
 
 pub(crate) static IS_INITIALIZED: RwLock<bool> = RwLock::new(false);
 pub(crate) static IS_ENABLED: AtomicBool = AtomicBool::new(false);
-pub(crate) static HANDLE: OnceCell<Mutex<Option<BackgroundServiceManager>>> = OnceCell::new();
+pub(crate) static HANDLE: OnceLock<Mutex<Option<BackgroundServiceManager>>> = OnceLock::new();
